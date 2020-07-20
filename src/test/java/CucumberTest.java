9b8e3d2ab1fe0a77d228f8b7b5a471ebf31cda95
Sergey Litvinov <litvinov.qa@gmail.com>
2020-07-20 17:09:02 +0300

    Конфигурирование Maven: параллельный и непараллельный запуск. Аллур отчет

---
+++ src/test/java/CucumberTest.java
@@ -0,0 +1,22 @@
+
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.SnippetType;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {"pretty"},
+        features = "src/test/resources/features", // путь к фичам
+        glue = "UIAutoTest", // пакет с шагами
+        tags = "@all", // тэги запуска
+        //dryRun = true,// признак проверки имплементации шагом перед прохождением теста
+         strict = true, // падать при нереализованном шаге?
+        snippets = SnippetType.CAMELCASE // в каком формате предлагать нереализованные шаги
+//      name = "^Успешное|Успешная.*" // фильтрация запускаемых тестов, у которых название удовлетворяет регулярному выражению
+)
+public class CucumberTest {
+
+}
