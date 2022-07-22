2b7fdecaafd866e3f801909bd829b138f8f0cd2a
Сергей <litvinov.qa@yandex.ru>
2022-07-22 20:32:59 +0300

    Больше драйверов и обновленный селениум

--- src/main/java/Tests/UI/actions/DriverActions.java
+++ src/main/java/Tests/UI/actions/DriverActions.java
@@ -4,13 +4,21 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
 
 public class DriverActions {
 
-    String driverType = "chrome";
-
+    /**
+     * Any following expected driver type should be installed local before
+     */
+    private final String CHROME = "chrome";
+    private final String SAFARI = "safari";
+    private final String FIREFOX = "firefox";
+    private final String EDGE = "edge";
+    String driverType = "firefox";
     private WebDriver driver = getDriverByType(driverType);
-
     public WebDriver getDriver() {
         return driver;
     }
@@ -18,16 +26,27 @@ public class DriverActions {
     public WebDriver getDriverByType(String driverType) {
         WebDriver driver;
         switch (driverType) {
-            case "chrome":
+            case CHROME:
                 WebDriverManager.chromedriver().setup();
                 driver = new ChromeDriver();
+                break;
+            case SAFARI:
+                WebDriverManager.safaridriver().setup();
+                driver = new SafariDriver();
+                break;
+            case FIREFOX:
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+                break;
+            case EDGE:
+                WebDriverManager.edgedriver().setup();
+                driver = new EdgeDriver();
                 break;
             default:
                 driver = null;
         }
         return driver;
     }
-
 
 
 }
