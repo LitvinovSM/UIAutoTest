c1e3885fcc4b59e6fecbd1c2536904eb590083b5
Сергей <litvinov.qa@yandex.ru>
2022-09-29 12:24:59 +0300

    убрал полностью allure отчет

--- src/main/java/autotests/UI/pages/main/MainActions.java
+++ src/main/java/autotests/UI/pages/main/MainActions.java
@@ -5,29 +5,24 @@ import io.cucumber.java.ru.И;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
-import java.util.HashMap;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class MainActions extends MainElements {
-
+    //@Step("'главная страница' открывает URL {URL}")
     @Дано("'главная страница' открывает URL {string}")
     public void openPageByURL(String URL){
         webDriver.get(URL);
         wait.until(ExpectedConditions.visibilityOf(app));
     }
-
-    @И("открывает телеграмм веб")
-    public void открываетТелеграммВеб() {
-    }
-
+    //@Step("'главная страница' проверяет наличие ссылки с текстом {linkText}")
     @И("'главная страница' проверяет наличие ссылки с текстом {string}")
     public void checkThatLinkWithTextExists(String linkText) {
         List<WebElement> targetElements = getLinksByText(linkText);
         assertTrue(targetElements.size()!=0, String.format("Ссылка с текстом %s не отображается на форма",linkText));
     }
-
+    //@Step("'главная страница' проверяет наличие ссылки с текстом {linkText} в элементе {elementKey}")
     @И("'главная страница' проверяет наличие ссылки с текстом {string} в элементе {string}")
     public void checkLinksByTextInElement(String linkText, String elementKey) {
         elementKey = elementKey.toUpperCase();
@@ -35,7 +30,7 @@ public class MainActions extends MainElements {
         WebElement targetElement = webElementMap.get(elementKey);
         WebElement link = getLinkByTextInElement(targetElement,linkText);
     }
-
+    //@Step("'главная страница' проверяет наличие кнопки с текстом {buttonText} в элементе {elementKey}")
     @И("'главная страница' проверяет наличие кнопки с текстом {string} в элементе {string}")
     public void checkButtonsByTextInElement(String buttonText, String elementKey) {
         elementKey = elementKey.toUpperCase();
